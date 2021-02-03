c3346ec185ead82eeb155805043d13e3f049243c
sogo7 <Sogohara7@icloud.com>
2021-02-03 23:35:12 +0900

    checkbox作成！

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,8 +19,12 @@ module SessionsHelper
 
     def current_user
         if (user_id = session[:user_id])
+            pp "これがsession"
+            pp session[:user_id]
         @current_user ||= User.find_by(id: user_id)
         elsif (user_id = cookies.signed[:user_id])
+            pp "これがcookie"
+            pp cookies.signed[:user_id]
           user = User.find_by(id: user_id)
           if user && user.authenticated?(cookies[:remember_token])
             log_in user
