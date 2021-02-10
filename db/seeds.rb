1997bf32ede39e5544e27870152d4cc23ca33758
sogo7 <Sogohara7@icloud.com>
2021-02-11 01:02:08 +0900

    11章終了！

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
+      email: "example@railstutorial.org",
+      password:              "foobar",
+      password_confirmation: "foobar",
+      admin: true)
+      activated: true,
+      activated_at: Time.zone.now)
+
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
@@ -10,7 +13,10 @@ User.create!(name:  "Example User",
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+      email: email,
+      password:              password,
+      password_confirmation: password)
+      activated: true,
+      activated_at: Time.zone.now)
+
 end
