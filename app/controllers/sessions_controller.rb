de102ae587544336bfd50ef108f88adb525d7b63
sogo7 <Sogohara7@icloud.com>
2021-02-13 23:19:53 +0900

    13章終了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
+      if 
     log_in user
     params[:session][:remember_me] == '1' ? remember(user) : forget(user)
     redirect_to_back_or user
